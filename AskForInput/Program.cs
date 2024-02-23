5a3fe82ecde60177401c34fd7871feb8ab35915d
AndreSebastiao <andresebastiao755@gmail.com>
2024-02-23 14:45:02 +0000

    Ask for a second input

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,7 +6,19 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Ask for Input
+            Console.WriteLine("Please submit a whole number");
+
+            //Read Input
+            string input = Console.ReadLine();
+
+            //Transform the input in to a int
+            int i = int.Parse(input);
+            Console.WriteLine($"this is in i: {i}");
+             
+            //Input of a new number
+            Console.WriteLine("Please summit a real number");
+            string input2 = Console.ReadLine();
         }
     }
 }
