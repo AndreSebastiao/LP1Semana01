32f7bd182435888483079f56a039af86c6bf51ef
AndreSebastiao <andresebastiao755@gmail.com>
2024-02-23 15:22:03 +0000

    Multiple inputs and output

--- CentraPelaVirgula/Program.cs
+++ CentraPelaVirgula/Program.cs
@@ -15,8 +15,26 @@ namespace CentraPelaVirgula
             string input1 = Console.ReadLine();
             float f1 = float.Parse(input1);
 
+            //Read the second input
+            Console.WriteLine("Please input the second real number");
+            string input2 = Console.ReadLine();
+            float f2 = float.Parse(input2);
+
+            //Read the third input
+            Console.WriteLine("Please input the third real number");
+            string input3 = Console.ReadLine();
+            float f3 = float.Parse(input2);
+
+            //Read the forth input
+            Console.WriteLine("Please input the forth real number");
+            string input4 = Console.ReadLine();
+            float f4 = float.Parse(input2);
+
             //Output the results
             Console.WriteLine($"'{f1,6:f2}'");
+            Console.WriteLine($"'{f2,6:f2}'");
+            Console.WriteLine($"'{f3,6:f2}'");
+            Console.WriteLine($"'{f4,6:f2}'");
         }
     }
 }
