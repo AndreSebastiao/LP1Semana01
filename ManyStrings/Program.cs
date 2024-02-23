0f13a59033f09891dde75b2f122d31e595abf798
AndreSebastiao <andresebastiao755@gmail.com>
2024-02-23 12:36:47 +0000

    Added interpolation and concatenation strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ManyStrings
 {
@@ -6,15 +7,23 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
+            //Variables
+            char u = '\u261E';
+            float x = 3.1415f;
+
             //String contents
             string a = "\u00A9\n\u261D isto é um copyright";
             string b = "\u25B2 isto é apenas uma mão a apontar para cima";
             string c = "ah, e aquilo era so uma seta para cima tambem";
+            string d = "Eu vou naquela " + u + " direção";
+            string e = "o valor de x é " + x;
 
             //Print of content
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
         }
     }
 }
