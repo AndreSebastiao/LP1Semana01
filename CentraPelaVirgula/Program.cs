5aac61f47245642f49936253735939e228d5797b
AndreSebastiao <andresebastiao755@gmail.com>
2024-02-23 15:21:09 +0000

    Creation of CentraPelaVirgula

---
+++ CentraPelaVirgula/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace CentraPelaVirgula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            //Read the first input
+            Console.WriteLine("Please input the first real number");
+            string input1 = Console.ReadLine();
+            float f1 = float.Parse(input1);
+
+            //Output the results
+            Console.WriteLine($"'{f1,6:f2}'");
+        }
+    }
+}
