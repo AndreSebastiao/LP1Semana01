1b0d186c478ca092be4f72924eee27ee29f5ef27
AndreSebastiao <andresebastiao755@gmail.com>
2024-02-23 12:24:23 +0000

    Created ManyStrings and added various strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String contents
+            string a = "\u00A9\n\u261D isto é um copyright";
+            string b = "\u25B2 isto é apenas uma mão a apontar para cima";
+            string c = "ah, e aquilo era so uma seta para cima tambem";
+
+            //Print of content
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
