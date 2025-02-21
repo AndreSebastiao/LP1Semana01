21305c471d2ed082e659cfc8515fd20fd210a061
AndreSebastiao <andresebastiao755@gmail.com>
2025-02-21 14:00:32 +0000

    Add documentation

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Class que comeca o programa
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Metodo que devolve as frases
+        /// </summary>
+        /// <param name="args">Frases</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
