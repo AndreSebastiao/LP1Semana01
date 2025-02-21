f3a2aea7e4ac287f7e73bb4241b6000d51bb59cc
AndreSebastiao <andresebastiao755@gmail.com>
2025-02-21 14:50:09 +0000

    números em formatações diferentes

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -2,10 +2,25 @@
 
 namespace ManyStrings
 {
+
     public class Program
     {
         private static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+
+            string s1 =$"{xx:f2}";
+            string s2 =$"{xx:p1}";
+
+            string s3 =$"{ii:x}";
+            string s4 =$"{ii:c}";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+
             string s ="Um tab\t e um copyright\u03A9" + 2;
             string t ="Isto é um teste";
 
