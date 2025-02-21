dd448a1ca0505bc4b6000e3362e97b65d6f1796f
AndreSebastiao <andresebastiao755@gmail.com>
2025-02-21 14:56:59 +0000

    Exercicio 7

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,7 +6,15 @@ namespace AskForInput
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Escreve um número inteiro:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Agora escreve um número real:");
+            float b = float.Parse(Console.ReadLine());
+
+
+            float c = a + b;
+            Console.WriteLine(c);
         }
     }
 }
